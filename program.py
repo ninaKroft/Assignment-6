b8e635c94b78d01e5198c5bcd9801c9dba3cd115
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-10 18:07:03 +0000

    text and negative number detection

--- program.py
+++ program.py
@@ -5,11 +5,20 @@ def calculations(number):
     x = 1
     z = 1
     pi = 0
-    for y in range(1, number + 1):
+    for y in range(1, int(number) + 1):
         pi = pi + (z / x)
         x = x + 2
         z = z * -1
     return pi * 4
 
-iterations = int(input())
-print(calculations(iterations))
+
+iterations = input()
+try:
+    int(iterations)
+except ValueError:
+    print("You entered text. Please enter a positive integer.")
+else:
+    if int(iterations) < 0:
+        print("You entered a negative number. Please enter a positive integer.")
+    else:
+        print(calculations(iterations))
