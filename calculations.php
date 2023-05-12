56cf21d34e59030cf71e3d8c8a52bf043133cef1
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-12 17:36:26 +0000

    text, - number, decimal detection + prevention

--- calculations.php
+++ calculations.php
@@ -9,6 +9,7 @@
 </head>
 <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
 <?php
     if (isset ($_POST['iterations'])){
         $iterations = $_POST['iterations'];
@@ -17,6 +18,8 @@
         $top = 1;
         $bottom = 1;
         $pi = 0;
+        $iterations = round($iterations, 0);
+        echo $iterations;
         for ($x = 0; $x < $iterations; $x++){
             $pi = $pi + ($top / $bottom);
             $bottom = $bottom + 2;
@@ -24,6 +27,26 @@
         }
         return $pi * 4;
         }
+
+        function checkNumber($iterations){
+            return is_numeric($iterations);
+        }
+
+        function checkNegative($iterations){
+            return $iterations < 0;
+        }
+
+        if (checkNumber($iterations) == False){
+            echo "You entered text. Please return to the main page and enter a positive integer.";
+            exit();
+        }
+
+        if (checkNegative($iterations) == True){
+            echo "You entered a negative number. Please return to the main page and enter a positive integer.";
+            exit();
+        }
+
+        
     echo calculations($iterations);
 
     }
