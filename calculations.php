51fe41346c4976db6b16823c700882c22e2e2c49
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-05-12 17:01:59 +0000

    bracketsss

--- calculations.php
+++ calculations.php
@@ -17,10 +17,11 @@
         $top = 1;
         $bottom = 1;
         $pi = 0;
-        for ($x = 0; $x < $iterations; $x++);
+        for ($x = 0; $x < $iterations; $x++){
             $pi = $pi + ($top / $bottom);
             $bottom = $bottom + 2;
             $top = $top * -1;
+        }
         return $pi * 4;
         }
     echo calculations($iterations);
